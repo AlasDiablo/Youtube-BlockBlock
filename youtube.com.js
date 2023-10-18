45b1b9a889b5d000b58d08e553d69ee3d18cf3ea
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-10-18 20:59:14 +0200

    feat: add a responsive player

--- youtube.com.js
+++ youtube.com.js
@@ -9,19 +9,30 @@ const observer = new MutationObserver(() => {
     const videoId = urlParams.get('v');
 
     if (player) {
-        if (player.children.length === 1 && player.children.item(0).id === 'blockblock-player') {
+        if (player.children.length === 1 && player.children.item(0).id === 'player-container-outer') {
             return;
         }
+        let url = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+        // TODO: Found a way to clean up params before applying it to the iframe
+        // if (urlParams.has('t')) {
+        //     url += `&start=${urlParams.get('t')}`;
+        // }
         player.innerHTML = `
-<iframe 
-    style="width: 100%; height: 35vh"
-    id="blockblock-player"
-    src="https://www.youtube.com/embed/${videoId}"
-    title="YouTube video player" 
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowfullscreen>
-</iframe>
+<div id="player-container-outer" class="style-scope ytd-watch-flexy">
+  <div id="player-container-inner" class="style-scope ytd-watch-flexy">
+    <div id="player-container" role="complementary" class="style-scope ytd-watch-flexy" style="touch-action: auto;">
+    <iframe 
+        style="width: 100%; height: 100%"
+        id="blockblock-player"
+        src="${url}"
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen>
+    </iframe>
+    </div>
+  </div>
+</div>
         `;
     }
 });
